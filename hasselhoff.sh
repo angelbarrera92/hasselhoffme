675d62130ac27e33cd958bb672b540d9b83e41a8
Angel Barrera <angelbarrerasanchez@gmail.com>
2018-04-11 20:30:49 +0200

    first version

---
+++ hasselhoff.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+################################
+## Platform: Gnome            ##
+################################
+
+
+## Preparing full bing url of image ##
+URL='https://media.cntraveller.in/wp-content/uploads/2016/08/davidhasselhofflead.jpg'
+
+## Local directory where this script file placed
+CURDIR="/tmp"
+
+## Local directory for images
+IMGDIR=$CURDIR"/david"
+
+## Creating directory if not present ##
+mkdir -m a=rwx -p $IMGDIR
+
+## Local path of output image ##
+LOCALIMG=$IMGDIR"/hasselhofflead.jpg"
+
+## Download image from bing to local path ##
+curl $URL --output $LOCALIMG
+
+## Setting desktop background image from local ##
+gsettings set org.gnome.desktop.background picture-uri 'file://'$LOCALIMG

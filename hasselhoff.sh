f537f3c074636b49c5c3ce2f586442641823a54a
Angel Barrera <angelbarrerasanchez@gmail.com>
2018-05-05 22:05:40 +0200

    removes comments

--- hasselhoff.sh
+++ hasselhoff.sh
@@ -16,15 +16,7 @@ echo ${machine}
 
 VERSION="0.0.2"
 AMD64_URL="https://github.com/angelbarrera92/hasselhoffme/releases/download/${VERSION}/hasselhoffme_${VERSION}_${machine}_amd64.tar.gz"
-
-## Local directory for binary
 BINDIR="/tmp/david"
-
-## Creating directory if not present ##
 mkdir -m a=rwx -p $BINDIR
-
-## Download the binary to local path ##
 cd $BINDIR && curl -s -L $AMD64_URL | tar xz
-
-## Setting desktop background image from using the binary ##
 $BINDIR/hasselhoffme

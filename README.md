b473061433da3f13d4ea9159b6ab1640397ffbee
ervitis <ervitis@users.noreply.github.com>
2018-06-04 07:50:46 +0200

    Use github api to get content from wallpaper folder (#7)
    * add dep package dependencies
    
    * use bing to search images and choose a random image
    
    * update readme style
    
    * update readme dependencies
    
    * update readme and travis for deploying
    
    * use environment variables for releasing
    
    * debug use repo name
    
    * use encrypted for deploy
    
    * setup build
    
    * use github provider
    
    * use goreleaser
    
    * update gitignore
    
    * delete build stage in travis
    
    * add file dependency go
    
    * change build main
    
    * use net library for parsing html
    
    * update readme
    
    * add gitraw and wallpapers
    
    * set wallpaper
    
    * fix name user repo and import relative path
    
    * delete comments in lock file
    
    * fix error importing
    
    * Update README.md
    
    * Update hasselhoffme.go
    
    * use github api to get contents
    
    * use github api
    
    * fix dep ensure
    
    * add more download url
    
    * use api to retrieve url content
    
    * Update README.md
    
    Removes non intuitive url

--- README.md
+++ README.md
@@ -6,5 +6,12 @@ wget http://tiny.cc/hasselhoff -O - | bash
 curl -L http://tiny.cc/hasselhoff | bash
 ```
 
+If you can't run the script, try with this url:
+```bash
+curl -L http://tiny.cc/hasselhoffme | bash
+```
+
 ## Tested
-Tested on fedora 27
+- Tested on fedora 27
+- MacOS
+- Windows
--- images/gitraw_images.go
+++ images/gitraw_images.go
@@ -2,14 +2,75 @@ package images
 
 import (
 	"fmt"
+	"net/http"
+	"encoding/json"
+	"io/ioutil"
 )
 
 const UserRepo = "angelbarrera92/hasselhoffme"
-const RawRepo = "https://raw.githubusercontent.com"
-const FoldersRepo = "master/wallpapers"
+
+type Links struct {
+	Self string `json:"message"`
+	Git string `json:"git"`
+	Html string `json:"html"`
+}
+
+type Content struct {
+	Name string `json:"name"`
+	Path string `json:"path"`
+	Sha string `json:"sha"`
+	Size int `json:"size"`
+	Url string `json:"url"`
+	HtmlUrl string `json:"html_url"`
+	GitUrl string `json:"git_url"`
+	DownloadUrl string `json:"download_url"`
+	TypeObject string `json:"type"`
+	Links Links `json:"links"`
+}
 
 func SearchGithubRawImages(w string) (result string) {
-	url := fmt.Sprintf("%s/%s/%s/", RawRepo, UserRepo, FoldersRepo)
+	url := fmt.Sprintf("https://api.github.com/repos/%s/contents/wallpapers", UserRepo)
 
-	return fmt.Sprintf("%s%d.jpg", url, RandomNumberInt(1, 9))
+	content, err := getContent(url)
+	if err != nil {
+		panic("Could not load any image")
+	}
+
+	var images []ImageResult
+
+	for k, v := range content {
+		images = append(images, ImageResult{
+			Source: v.DownloadUrl,
+			Index: k,
+		})
+	}
+
+	rn := RandomNumber(images)
+
+	return images[rn].Source
+}
+
+func getContent(baseUrl string) ([]Content, error) {
+
+	var content []Content
+
+	resp, _ := http.Get(baseUrl)
+	if resp.StatusCode >= 400 {
+		return nil, fmt.Errorf("error response: %s", resp.Body)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	err = json.Unmarshal(body, &content)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	return content, nil
 }

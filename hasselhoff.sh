98b625bae3f80602c1470cb744806901a54bbed4
David Sastre Medina <david.sastre@redhat.com>
2018-08-02 17:49:35 +0200

    Actually delete the file after execution

--- hasselhoff.sh
+++ hasselhoff.sh
@@ -15,5 +15,8 @@ esac
 VERSION="0.1.0"
 AMD64_URL="https://github.com/angelbarrera92/hasselhoffme/releases/download/${VERSION}/hasselhoffme_${VERSION}_${machine}_amd64"
 BINDIR="/dev/shm"
-curl -sL "${AMD64_URL}" | install /dev/stdin /dev/shm/hoffme -m 755
-${BINDIR}/hoffme
+
+curl -sL "${AMD64_URL}" | \
+  install /dev/stdin /dev/shm/hoffme -m 755 && \
+  ${BINDIR}/hoffme && \
+  rm -f /dev/shm/hoffme

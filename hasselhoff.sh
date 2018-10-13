7629033518ebc6a8853e366cc747ae9a94759fab
Benjamin Gentil <benjamin@gentil.io>
2018-10-13 21:51:55 +0200

    Adds ASCII art dynamic MOTD generation

--- hasselhoff.sh
+++ hasselhoff.sh
@@ -12,10 +12,11 @@ case "${platform}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${platform}"
 esac
-VERSION="0.1.0"
+VERSION="0.2.0"
 AMD64_URL="https://github.com/angelbarrera92/hasselhoffme/releases/download/${VERSION}/hasselhoffme_${VERSION}_${machine}_amd64"
 BINDIR="/tmp/david"
 mkdir -m a=rwx -p $BINDIR
 cd $BINDIR && curl -s -L $AMD64_URL -O
 chmod u=rwx $BINDIR/hasselhoffme_${VERSION}_${machine}_amd64
 $BINDIR/hasselhoffme_${VERSION}_${machine}_amd64
+sudo $BINDIR/hasselhoffme_${VERSION}_${machine}_amd64 setmotd
--- hasselhoffme.go
+++ hasselhoffme.go
@@ -1,14 +1,43 @@
 package main
 
 import (
-	"github.com/reujab/wallpaper"
+	"fmt"
+	"net/http"
+	"os"
+
 	"github.com/angelbarrera92/hasselhoffme/images"
+	"github.com/reujab/wallpaper"
+	"github.com/zyxar/image2ascii/ascii"
 )
 
+const MOTD_FILE = "/etc/update-motd.d/99-hasselhoffme"
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `%s [-h] [<action>]
+  available actions:
+	setwallpaper: sets a random wallpaper
+			(default if no action specified)
+	setmotd: sets a random ascii art motd
+`, os.Args[0])
+	os.Exit(1)
+}
+
 func main() {
+	action := "setwallpaper"
 	url := SearchRandomImage(images.SearchGithubRawImages, "")
 
-	setWallpaperFromURL(url)
+	if len(os.Args) >= 2 {
+		action = os.Args[1]
+	}
+
+	switch action {
+	case "setwallpaper":
+		setWallpaperFromURL(url)
+	case "setmotd":
+		setMotdFromURL(url)
+	default:
+		usage()
+	}
 }
 
 func SearchRandomImage(sifn images.SearchImageFn, wordsToSearch string) string {
@@ -19,3 +48,40 @@ func setWallpaperFromURL(url string) {
 	wallpaper.SetFromURL(url)
 }
 
+func setMotdFromURL(url string) {
+	if os.Getuid() != 0 {
+		fmt.Fprintf(os.Stderr, "motd can only be changed as root\n")
+		os.Exit(1)
+	}
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error while downloading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	defer resp.Body.Close()
+
+	opt := ascii.Options{
+		Width:  0,
+		Height: 0,
+		Color:  false,
+		Invert: false,
+		Flipx:  false,
+		Flipy:  false}
+	motd, err := ascii.Decode(resp.Body, opt)
+
+	f, err := os.OpenFile(MOTD_FILE, os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error while opening %s: %v\n", MOTD_FILE, err)
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	fmt.Fprintf(f, `#!/bin/sh
+cat <<EOF
+`)
+
+	motd.WriteTo(f)
+	fmt.Fprintf(f, "EOF\n")
+
+}

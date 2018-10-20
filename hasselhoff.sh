1aff30517a2d2ce104cf24897b889371122c8e77
Ahmy Yulrizka <yulrizka@users.noreply.github.com>
2018-10-20 21:21:14 +0200

    add arm detection using uname -m
    unfortunately v5 is not returned by uname
    https://en.wikipedia.org/wiki/Uname

--- hasselhoff.sh
+++ hasselhoff.sh
@@ -12,14 +12,20 @@ case "${platform}" in
     MINGW*)     machine=MinGw;;
     *)          machine="UNKNOWN:${platform}"
 esac
+arch="$(uname -m)"
+case "${arch}" in
+    armv6l)    arch=armv6;;
+    armv7l)    arch=armv7;;
+    *)         arch="amd64"
+esac
 VERSION="0.2.2"
-AMD64_URL="https://github.com/angelbarrera92/hasselhoffme/releases/download/${VERSION}/hasselhoffme_${VERSION}_${machine}_amd64"
+BIN_URL="https://github.com/angelbarrera92/hasselhoffme/releases/download/${VERSION}/hasselhoffme_${VERSION}_${machine}_${arch}"
 BINDIR="/tmp/david"
 mkdir -m a=rwx -p $BINDIR
-cd $BINDIR && curl -s -L $AMD64_URL -O
-chmod u=rwx $BINDIR/hasselhoffme_${VERSION}_${machine}_amd64
+cd $BINDIR && curl -s -L $BIN_URL -O
+chmod u=rwx $BINDIR/hasselhoffme_${VERSION}_${machine}_${arch}
 if [ -z "$DISPLAY" ] && [ "$machine" != "darwin" ];then
-    sudo $BINDIR/hasselhoffme_${VERSION}_${machine}_amd64 setmotd
+    sudo $BINDIR/hasselhoffme_${VERSION}_${machine}_${arch} setmotd
 else
-    $BINDIR/hasselhoffme_${VERSION}_${machine}_amd64
+    $BINDIR/hasselhoffme_${VERSION}_${machine}_${arch}
 fi

f9e3489d62321d2f1921aa5157f7d793191ee881
Angel Barrera <angelbarrerasanchez@gmail.com>
2018-10-20 14:34:31 +0200

    updates scripts to download latests version

--- hasselhoff.cmd
+++ hasselhoff.cmd
@@ -1,6 +1,6 @@
 @echo off
 echo "Calling the Hasselhoff..."
-SET VERSION=0.1.0
+SET VERSION=0.2.1
 
 curl -s -L https://github.com/angelbarrera92/hasselhoffme/releases/download/%VERSION%/hasselhoffme_%VERSION%_windows_amd64.exe --output hass.exe > NUL
 

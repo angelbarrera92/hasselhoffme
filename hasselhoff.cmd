63641bbf483ea02d487cbd76d15dca6d9bcfeea4
Kah Shing <kahshingchee@gmail.com>
2018-10-25 09:59:01 +0800

    clear downloaded binary

--- hasselhoff.cmd
+++ hasselhoff.cmd
@@ -1,8 +1,10 @@
 @echo off
 echo "Calling the Hasselhoff..."
 SET VERSION=0.2.2
+SET curr=%cd%
 
 curl -s -L https://github.com/angelbarrera92/hasselhoffme/releases/download/%VERSION%/hasselhoffme_%VERSION%_windows_amd64.exe --output hass.exe > NUL
 
 echo "Hasselhoffing..."
 hass.exe
+del /F "%curr%\hass.exe"
